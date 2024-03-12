885b99650f29c9f856650cbbd4b431c0f5f423cd
Mariaamy <mariaamy@stud.ntnu.no>
2024-03-12 12:15:48 +0100

    fix: module warnings

---
+++ app/config-overrides.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+
+// https://stackoverflow.com/questions/63195843/webpack-module-warning-failed-to-parse-source-map-from-data-url
+
+module.exports = function override(config) {
+
+    config.ignoreWarnings = [/Failed to parse source map/];
+    const fallback = config.resolve.fallback || {};
+    Object.assign(fallback, {
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "assert": require.resolve("assert"),
+        "http": require.resolve("stream-http"),
+        "https": require.resolve("https-browserify"),
+        "os": require.resolve("os-browserify"),
+        "url": require.resolve("url")
+    })
+
+    config.resolve.fallback = fallback;
+    config.plugins = (config.plugins || []).concat([
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+            Buffer: ['buffer', 'Buffer']
+        })
+    ])
+    return config;
+}
